81e4fceb298133a656d64012257f94a5125fc853
P1xt <P1xt@users.noreply.github.com>
2018-05-07 18:09:23 -0500

    Generate the Home page.

--- client/src/App/Content/Content.js
+++ client/src/App/Content/Content.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { ProjectRoutes } from "../../Projects";
+import { Route } from "react-router-dom";
+
+import { Home } from "../../Home";
 
 const Content = () => {
   return (
     <div>
+      <Route exact path="/" component={Home} />
       <ProjectRoutes />
     </div>
   );
