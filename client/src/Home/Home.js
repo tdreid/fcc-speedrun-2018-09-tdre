aa4dde40b57f3430bf4d4d20dc3d65407410840f
P1xt <P1xt@users.noreply.github.com>
2018-05-08 09:50:45 -0500

    Remove project images on home page.

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {
   Card,
-  CardImg,
   CardText,
   CardBody,
   CardTitle,
@@ -19,12 +18,6 @@ const Home = () => {
       </div>
       <div className="Home_contents">
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Tribute Page Project"
-          />
           <CardBody>
             <CardTitle>Tribute Page</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -40,12 +33,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Personal Portfolio Webpage Project"
-          />
           <CardBody>
             <CardTitle>Personal Portfolio Webpage</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -61,12 +48,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Random Quote Machine Project"
-          />
           <CardBody>
             <CardTitle>Random Quote Machine</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -82,12 +63,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Local Weather Project"
-          />
           <CardBody>
             <CardTitle>Local Weather</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -103,12 +78,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Wikipedia Viewer Project"
-          />
           <CardBody>
             <CardTitle>Wikipedia Viewer</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -124,12 +93,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Twitchtv Project"
-          />
           <CardBody>
             <CardTitle>Twitchtv</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -145,12 +108,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Calculator Project"
-          />
           <CardBody>
             <CardTitle>Calculator</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -166,12 +123,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Pomodoro Clock Project"
-          />
           <CardBody>
             <CardTitle>Pomodoro Clock</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -187,12 +138,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Tic Tac Toe Game Project"
-          />
           <CardBody>
             <CardTitle>Tic Tac Toe Game</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -208,12 +153,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Simon Game Project"
-          />
           <CardBody>
             <CardTitle>Simon Game</CardTitle>
             <CardSubtitle>FreeCodeCamp Frontend Project</CardSubtitle>
@@ -229,12 +168,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Markdown Previewer Project"
-          />
           <CardBody>
             <CardTitle>Markdown Previewer</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -250,12 +183,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Camper Leaderboard Project"
-          />
           <CardBody>
             <CardTitle>Camper Leaderboard</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -271,12 +198,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Recipe Box Project"
-          />
           <CardBody>
             <CardTitle>Recipe Box</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -292,12 +213,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Gameof Live Project"
-          />
           <CardBody>
             <CardTitle>Gameof Live</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -313,12 +228,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Roguelike Game Project"
-          />
           <CardBody>
             <CardTitle>Roguelike Game</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -334,12 +243,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Bar Chart Project"
-          />
           <CardBody>
             <CardTitle>Bar Chart</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -355,12 +258,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Scatterplot Graph Project"
-          />
           <CardBody>
             <CardTitle>Scatterplot Graph</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -376,12 +273,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Heat Map Project"
-          />
           <CardBody>
             <CardTitle>Heat Map</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -397,12 +288,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Force Directed Graph Project"
-          />
           <CardBody>
             <CardTitle>Force Directed Graph</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -418,12 +303,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Data Acrossthe Globe Project"
-          />
           <CardBody>
             <CardTitle>Data Acrossthe Globe</CardTitle>
             <CardSubtitle>FreeCodeCamp Datavis Project</CardSubtitle>
@@ -439,12 +318,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Timestamp Microservice Project"
-          />
           <CardBody>
             <CardTitle>Timestamp Microservice</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -460,12 +333,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Request Header Parser Project"
-          />
           <CardBody>
             <CardTitle>Request Header Parser</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -481,12 +348,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of U R L Shortener Project"
-          />
           <CardBody>
             <CardTitle>U R L Shortener</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -502,12 +363,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Image Search Project"
-          />
           <CardBody>
             <CardTitle>Image Search</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -523,12 +378,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of File Metadata Project"
-          />
           <CardBody>
             <CardTitle>File Metadata</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -544,12 +393,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Voting App Project"
-          />
           <CardBody>
             <CardTitle>Voting App</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -565,12 +408,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Nightlife Coordination Project"
-          />
           <CardBody>
             <CardTitle>Nightlife Coordination</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -586,12 +423,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Stock Market Project"
-          />
           <CardBody>
             <CardTitle>Stock Market</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -607,12 +438,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Book Trading Club Project"
-          />
           <CardBody>
             <CardTitle>Book Trading Club</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
@@ -628,12 +453,6 @@ const Home = () => {
           </CardBody>
         </Card>
         <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Image of Pinterest Clone Project"
-          />
           <CardBody>
             <CardTitle>Pinterest Clone</CardTitle>
             <CardSubtitle>FreeCodeCamp Backend Project</CardSubtitle>
