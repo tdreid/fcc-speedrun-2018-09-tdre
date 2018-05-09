6bf2b9872e00110de8e4ded00a36ab76b41ec16f
P1xt <P1xt@users.noreply.github.com>
2018-05-09 17:14:22 -0500

    Implemented Twitch.tv.
    Todo: come back and add tests.

--- client/src/Projects/Frontend/Twitchtv/Twitchtv.js
+++ client/src/Projects/Frontend/Twitchtv/Twitchtv.js
@@ -1,20 +1,123 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-
+import React, { Component } from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import "./Twitchtv.css";
+/**
+ * Displays a list of Twitch.tv users, including
+ * their avatar, user name, and current stream/offline
+ * @class Twitchtv
+ * @extends Component
+ */
+class Twitchtv extends Component {
+  constructor() {
+    super();
 
-const Twitchtv = () => {
-  return (
-    <div className="Twitchtv">
-      <Container>
+    this.state = {
+      streamersData: []
+    };
+
+    this.fetchStreams();
+  }
+
+  /**
+   * fetchStream
+   * loops through list of streamers
+   * and fetches both their user and their
+   * stream information from the Twitch API
+   * @memberof Twitchtv
+   */
+  fetchStreams = async () => {
+    await [
+      "ESL_SC2",
+      "OgamingSC2",
+      "cretetion",
+      "freecodecamp",
+      "storbeck",
+      "habathcx",
+      "RobotCaleb",
+      "noobs2ninjas"
+    ].map(async streamer => this.fetchStreamer(streamer));
+  };
+
+  /**
+   * fetchStreamer
+   * fetch a single Twitch user's information
+   * @memberof Twitchtv
+   */
+  fetchStreamer = async streamer => {
+    const API = "https://wind-bow.glitch.me/twitch-api";
+
+    // get the streamers
+    const userResponse = await fetch(API + "/users/" + streamer);
+    const userData = await userResponse.json();
+
+    // get the streamers associated streams
+    const streamResponse = await fetch(API + "/streams/" + streamer);
+    const streamData = await streamResponse.json();
+
+    this.setState({
+      streamersData: [...this.state.streamersData, { streamData, userData }]
+    });
+  };
+
+  /**
+   * render
+   * render the Twitchtv component
+   * @return
+   * @memberof Twitchtv
+   */
+  render() {
+    return (
+      <Container className="Twitchtv">
         <Row>
           <Col>
-            <h1>Twitchtv</h1>
+            <h2>Twitch App</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup>
+              {this.state.streamersData &&
+                this.state.streamersData
+                  .sort(
+                    (a, b) =>
+                      (a.streamData.stream === null) -
+                      (b.streamData.stream === null)
+                  )
+                  .map(stream => (
+                    <ListGroupItem key={stream.userData.display_name}>
+                      {" "}
+                      {stream.userData.logo !== null ? (
+                        <img alt="avatar" src={stream.userData.logo} />
+                      ) : (
+                        <img
+                          alt="missing avatar"
+                          src="http://via.placeholder.com/50x50"
+                        />
+                      )}
+                      {stream.userData.display_name}
+                      {stream.streamData.stream &&
+                      stream.streamData.stream.game !== null ? (
+                        <span>
+                          <a
+                            href={
+                              "https://www.twitch.tv/" + stream.userData.name
+                            }
+                            target="_blank"
+                          >
+                            {stream.streamData.stream.game}
+                          </a>
+                        </span>
+                      ) : (
+                        <span>Offline</span>
+                      )}
+                    </ListGroupItem>
+                  ))}
+            </ListGroup>
           </Col>
         </Row>
       </Container>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Twitchtv;
