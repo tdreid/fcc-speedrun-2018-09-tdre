fab42fdcf0b968f4f6db09fa3fe6379f52e4c2c7
P1xt <P1xt@users.noreply.github.com>
2018-05-12 11:20:31 -0500

    Implement Markdown Previewer.

--- client/src/Projects/Datavis/MarkdownPreviewer/MarkdownPreviewer.js
+++ client/src/Projects/Datavis/MarkdownPreviewer/MarkdownPreviewer.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+import marked from "marked";
 
 import "./MarkdownPreviewer.css";
 
-const MarkdownPreviewer = () => {
-  return (
-    <div className="MarkdownPreviewer">
-      <Container>
-        <Row>
-          <Col>
-            <h1>Markdown Previewer</h1>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+class MarkdownPreviewer extends Component {
+  constructor() {
+    super();
+    const text = "# Put markdown here";
+    this.state = {
+      text,
+      markdown: this.convertMarkdown(text)
+    };
+  }
+  textChanged = event => {
+    const text = event.target.value;
+    const markdown = this.convertMarkdown(text);
+    this.setState({ text, markdown });
+  };
+  convertMarkdown = text => {
+    marked.setOptions({ sanitize: true });
+    return { __html: marked(text) };
+  };
+  render() {
+    return (
+      <div className="MarkdownPreviewer">
+        <Container>
+          <Row>
+            <Col xs="12" sm="6">
+              <h2>Enter Markdown:</h2>
+              <hr />
+              <div className="MarkdownPreviewer_input">
+                <textarea
+                  autoFocus={true}
+                  className="textInput form-control"
+                  value={this.state.text}
+                  onChange={this.textChanged}
+                />
+              </div>
+            </Col>
+            <Col xs="12" sm="6">
+              <h2>Formatted Result:</h2>
+              <hr />
+              <div className="MarkdownPreviewer_output">
+                <div dangerouslySetInnerHTML={this.state.markdown} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default MarkdownPreviewer;
